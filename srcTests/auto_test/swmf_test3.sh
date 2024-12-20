1359a7168f78e439e9e7296a7f6e82c1cd889d8a
Aaron Bukowski <abukowski21@gmail.com>
2024-12-20 12:31:56 -0500

    TEST: Improve the functionality, and help messages, of the script to auto-test SWMF test3

--- srcTests/auto_test/swmf_test3.sh
+++ srcTests/auto_test/swmf_test3.sh
@@ -8,13 +8,14 @@ get_help(){
 
 > This script will automatically run SWMF test3 (UA coupling)
 > Just input a directory where to put the files, and the rest will be automatic.
-
+> You can either move this script & it will clone GITM, or if it is run from
+  GITM/srcTests/auto_test, it will copy the version of GITM to inside the SWMF.
 
 ------------------------------------------------------------------------------------
 > Example workflows (All using the -debug flag):
 -------------------
 
-- You just made changes on a GITM branch "couplefix", which have been pushed to
+- You just made changes on a GITM branch 'couplefix', which have been pushed to
   GITMCode/GITM, and *have not run this before*.
   - Assuming standalone GITM and SWMF will go into ~/Documents:
   > pwd
@@ -61,38 +62,49 @@ Arguments:
         --skip_config             Skip running Config.pl?
         --pull                    Run a 'gitall pull', updating the SWMF and GITM repos?
         (-b, --branch)  NAME      Branch name to pull from (on GITMCode/GITM.git).
-                                  - If left empty, "develop" is used. Branch is only
-                                    used it --pull is used as well, otherwise GITM
+                                  - If left empty, 'develop' is used. Branch is only
+                                    used if --pull is used as well, otherwise GITM
                                     is left in place!
-        (-O/--omax)     #         Optimization level. Use a space, please.
+        (-O/--omax)     #         Optimization level & just_compile. Use a space, please.
                                   - Example:  '-O 0' for fastest compilation.
                                     If this is used, we will not run test3. Just compile.
+				  - By default, -O3 is used in most cases. Set this to '-O 3' to
+				    'just_compile' and exit, not running test3.
         
         [-p/--path]     PATH      Path to clone SWMF to and run from. Required!
                                   - This can be an existing path.
                                   - -p/--path flags optional, but if no arg is used, 
                                     PATH is assumed to be the last argument.
+				  - If the path provided is not set with -p/--path, and the
+				    path does not exist,the script will print help & exit.
 
 
-(This is tested, but may not be free from bugs. Commit & push before running.)
+(This is tested, but may not be free from bugs. Commit & push, or backup, before running.)
 (Reach out with questions/comments/problems.)
 
 "
 
-exit 1
 }
 
 do_tests(){
     # setup run directory
-    
-    cd $testdir
 
+    # First, check to see if we're running from GITM's test folder
+    if [ -d ../../../GITM ]; then
+	# If so, get GITM's absolute path for copying later
+	gitm_path=$(pwd)/../../../GITM/
+    fi
+
+    cd $testdir
     if [ -d SWMF/ ]; then cd SWMF; fi
 
     # Check if we're inside swmf folder with git status:
+    # If this command fails, we are not in a git repo.
+    # This check is SO hacky and is not guaranteed, but it's good enough.
     git status --porcelain
-    if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ] && [ $gitm_path /= false] ; then
       # We're probably within SWMF... Don't need to clone.
+      # GITM folder *should* already exist
       if [ dopull == true ]; then
         echo "Checking out GITM branch $branch"
         sleep 2
@@ -109,25 +121,25 @@ do_tests(){
       # Probably not inside an existing SWMF folder...
       git clone --depth 1 git@github.com:SWMFSoftware/SWMF.git
       cd SWMF
+      if [ gitm_path /= false ]; then cp $gitm_path UA/ ; fi
+      # Config now, no prob config-ing later too
+      ./Config.pl -install $debug $OFlags -v=UA/GITM
     fi
 
-
-    
+    # Go thru options:
     if [ $config = true ]; then
         if [ -f Makefile.def ]; then
-            # Just make clean if running config, don't uninstall.
+            # Only clean if running config, don't uninstall.
             make clean
         fi
-
         ./Config.pl -install $debug $OFlags -v=UA/GITM
     fi
     
-
-
     if [ $clean = true ]; then
       make clean
     fi
 
+    # run test3
     if [ just_compile = true ]; then
       make -j 
     else
@@ -160,8 +172,11 @@ just_compile=false
 
 testdir=""
 
+# not set by user; just used by this script
+path_set_explicit=false
+gitm_path=false
 
-while [[ $# -gt 1 ]]; do
+while [[ $# -gt 0 ]]; do
   case "$1" in
     -h|--help)
       get_help
@@ -183,38 +198,47 @@ while [[ $# -gt 1 ]]; do
       shift
       ;;
     -O|--omax)
-      echo "Using max. optimization level -O$2"
+      echo "Using optimization level -O$2"
       OFlags="-O$2"
       just_compile=true
       shift 2
       ;;
     -b|--branch)
-      echo "Using GITM branch names $2"
+      echo "Using GITM branch named $2"
       branch=$2
       shift 2
       ;;
     -p|--path)
-      echo "Using GITM branch names $2"
-      branch=$2
+      echo "Using the provided path: $2"
+      testdir=$2
+      path_set_explicit=true
       shift 2
       ;;
+    *)
+      testdir=$1
+      shift
+      ;;
   esac
-
+  shift
 done
 
-## Get directory. remaining argument if not set already
-if [ $# -gt 0 ]; then
-  if [[ -d $1 ]]; then
-    echo "Found directory $1"
-    testdir=$1
-  else
-    echo "Directory $1 not found! Exiting."
-    echo "Found remaining $# arguments"
-    echo "There could be a probelm with your arguments given!"
-    echo "Try again please. Bye."
-    exit 1
-  fi
+## Check if directory exists
+if [[ -d $testdir ]]; then
+  echo "Found directory: " $testdir
+else
+    if [ $path_set_explicit == true ]; then
+       echo "Making directory: " testdir
+       mkdir -p $testdir
+    else
+	get_help
+	echo
+	echo "=>> Provided directory: " $testdir " does not exist!"
+	echo "    Refusing to make unless specified by --path."
+	echo "Exiting!"
+	exit 1
+    fi
 fi
+
 
 echo "Waiting 3 seconds then starting."
 printf "
