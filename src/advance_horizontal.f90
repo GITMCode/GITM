3cba7df4d6e2772a7c3321da335369474eca41b6
Aaron James Ridley <ridley@umich.edu>
2025-05-08 08:31:29 -0400

    BUG: report altitude of bad point

--- src/advance_horizontal.f90
+++ src/advance_horizontal.f90
@@ -360,7 +360,7 @@ subroutine advance_horizontal(iBlock)
           IsFound = .false.
           do iSpecies = 1, nSpecies
             if (NewNum_CV(iLon, iLat, iSpecies) < 0.0) then
-              write(*, *) "Species : ", iSpecies, iLon, iLat, iBlock
+              write(*, *) "Species : ", iSpecies, iLon, iLat, iAlt, iBlock
               stop
               NewNum_CV(iLon, iLat, iSpecies) = 1.0
               IsFound = .true.
