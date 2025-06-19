8f8e8a8a25b0d8ee6fc653c449cd6c2c0c2dabd8
Aaron Bukowski <abukowski21@gmail.com>
2025-06-19 11:57:56 -0400

    BUG: Version was not correctly read by all compilers

--- src/ModGITMVersion.f90
+++ src/ModGITMVersion.f90
@@ -1,12 +1,11 @@
 module ModGITMVersion
 
-  ! Version information is put insto src/.version when compiling GITM
+  ! Version variable is put into src/.version when compiling GITM
   ! Format is:
   ! (last commit date)_(commit hash)_(# of files changed from HEAD)
   ! - Gives info on which commit someone is working off of,
   !   and how many files were changed from that.
 
-  character(25), parameter :: GitmVersion = &
-                              INCLUDE ".version"
+  INCLUDE ".version"
 
 end module ModGITMVersion
