05897f1034da1d36db2fa12200dd12afd2135b11
Aaron Bukowski <abukowski21@gmail.com>
2026-04-24 13:28:06 -0400

    Fill in NetCDF coordinate data
    - Add time to coordinates (also still listed as attr)
    - Rename alt -> z
    - 1D arrays for lon, lat, z all have data & match expectations.
    - Does not work for mag coords or HME yet

--- src/ModOutputNetCDF.F90
+++ src/ModOutputNetCDF.F90
@@ -45,8 +45,8 @@ module ModOutputNetCDF
 
 #ifdef HavePNetCDF
   integer :: ncid = -1       ! PnetCDF file handle; -1 = no file open
-  integer, allocatable :: varids(:)  ! variable IDs for all data variables
-
+  integer, allocatable :: varids(:), & ! variable IDs for all data variables
+                          coordids(:) ! ID's for coordinates (1-D data)
   ! Cached from open_file for use in write_block
   integer :: nc_nBlocksLon = 0
   integer :: nc_nDims = 3    ! spatial dims of open file: 2 or 3
@@ -67,7 +67,9 @@ contains
     use ModSizeGitm, only: nLons, nLats, nAlts
     use ModElectrodynamics, only: nMagLons, nMagLats
     use ModOutputRegistry, only: find_output_type, RegisteredTypes
-    use ModTime, only: iTimeArray
+    use ModTime, only: iTimeArray, CurrentTime
+    ! Not compiled in standalone. Change 1965 when it is!
+    ! use ModTimeConvert, only: iYearBase 
 #endif
     character(len=*), intent(in) :: dir, cType, cTime
     integer, intent(in) :: cL
@@ -75,10 +77,14 @@ contains
 #ifdef HavePNetCDF
     character(len=300) :: filename
     integer :: iTypeIdx, nVars, nDims, iV, ierr, ndimid
-    integer :: dimid_lon, dimid_lat, dimid_alt, dimid_vars(3)
+    integer :: dimid_lon, dimid_lat, dimid_alt, dimid_time, dimid_vars(3)
+    integer :: varid_time
     integer(kind=MPI_OFFSET_KIND) :: nLons_g, nLats_g, nAlts_g, attlen
+    integer(kind=MPI_OFFSET_KIND) :: tstart(1), tcnt(1)
     character(len=80) :: varname
-    character(len=30) :: time_str
+    character(len=30) :: coord_str
+    character(len=40) :: coord_units
+    real(kind=8) :: time_val(1)
 
     ! Build filename
     filename = trim(dir)//"/"//trim(cType)//"_"//cTime(1:cL)//".nc"
@@ -128,15 +134,16 @@ contains
     ierr = nfmpi_def_dim(ncid, "lon", nLons_g, dimid_lon)
     ierr = nfmpi_def_dim(ncid, "lat", nLats_g, dimid_lat)
     if (nDims == 3) then
-      ierr = nfmpi_def_dim(ncid, "alt", nAlts_g, dimid_alt)
+      ierr = nfmpi_def_dim(ncid, "z", nAlts_g, dimid_alt)
     endif
+    ierr = nfmpi_def_dim(ncid, "time", 1_MPI_OFFSET_KIND, dimid_time)
 
-    ! --- Global time attribute ---
-    write(time_str, '(i4.4,"-",i2.2,"-",i2.2,"T",i2.2,":",i2.2,":",i2.2,"Z")') &
+    ! --- Global time attribute (ISO string, for human readability) ---
+    write(coord_str, '(i4.4,"-",i2.2,"-",i2.2,"T",i2.2,":",i2.2,":",i2.2,"Z")') &
       iTimeArray(1), iTimeArray(2), iTimeArray(3), &
       iTimeArray(4), iTimeArray(5), iTimeArray(6)
-    attlen = int(len_trim(time_str), MPI_OFFSET_KIND)
-    ierr = nfmpi_put_att_text(ncid, NF_GLOBAL, "time", attlen, trim(time_str))
+    attlen = int(len_trim(coord_str), MPI_OFFSET_KIND)
+    ierr = nfmpi_put_att_text(ncid, NF_GLOBAL, "time", attlen, trim(coord_str))
 
     ! --- Dimension IDs for data variables ---
     ! All registered vars (including Longitude/Latitude/Altitude from add_coord_vars)
@@ -153,8 +160,8 @@ contains
       dimid_vars(2) = dimid_lat
     endif
 
-    if (allocated(varids)) deallocate(varids)
-    allocate(varids(nVars))
+    if (allocated(varids)) deallocate(varids, coordids)
+    allocate(varids(nVars), coordids(ndimid))
 
     do iV = 1, nVars
       varname = trim(RegisteredTypes(iTypeIdx)%vars(iV)%shortName)
@@ -171,10 +178,44 @@ contains
       endif
     enddo
 
+    ! --- Coordinate variables  ----- !
+    ! Time (CF-compliant, enables xarray decode_times) ---
+    write(coord_units, '("seconds since ",i4.4,"-01-01 00:00:00")') 1965
+    ierr = nfmpi_def_var(ncid, "time", NF_DOUBLE, 1, [dimid_time], varid_time)
+    attlen = int(len_trim(coord_units), MPI_OFFSET_KIND)
+    ierr = nfmpi_put_att_text(ncid, varid_time, "units", attlen, trim(coord_units))
+    attlen = 8_MPI_OFFSET_KIND
+    ierr = nfmpi_put_att_text(ncid, varid_time, "calendar", attlen, "standard")
+    ! longitude
+    write(coord_units, '("degrees_east")')
+    ierr = nfmpi_def_var(ncid, "lon", NF_DOUBLE, 1, [dimid_lon], coordids(1))
+    attlen = int(len_trim(coord_units), MPI_OFFSET_KIND)
+    ierr = nfmpi_put_att_text(ncid, coordids(1), "units", attlen, trim(coord_units))
+    ! latitude
+    write(coord_units, '("degrees_north")')
+    ierr = nfmpi_def_var(ncid, "lat", NF_DOUBLE, 1, [dimid_lat], coordids(2))
+    attlen = int(len_trim(coord_units), MPI_OFFSET_KIND)
+    ierr = nfmpi_put_att_text(ncid, coordids(2), "units", attlen, trim(coord_units))
+    ! Altitude
+    if (nDims == 3) then
+      ierr = nfmpi_def_var(ncid, "z", NF_DOUBLE, 1, [dimid_alt], coordids(3))
+      attlen = 2_MPI_OFFSET_KIND
+      ierr = nfmpi_put_att_text(ncid, coordids(3), "units", attlen, "km")
+    endif
     ! --- End define mode (collective) ---
     ierr = nfmpi_enddef(ncid)
     if (ierr /= NF_NOERR) &
       write(*, *) "ModOutputNetCDF: nfmpi_enddef failed: ", nfmpi_strerror(ierr)
+
+    ! --------- Write time coordinate --------- !
+    ! Since all ranks hold the same CurrentTime), write happens here.
+    ! Other coords are written later
+    tstart(1) = 1_MPI_OFFSET_KIND
+    tcnt(1) = 1_MPI_OFFSET_KIND
+    time_val(1) = CurrentTime
+    ierr = nfmpi_put_vara_double_all(ncid, varid_time, tstart, tcnt, time_val)
+    if (ierr /= NF_NOERR) &
+      write(*, *) "ModOutputNetCDF: failed to write time variable: ", nfmpi_strerror(ierr)
 
     ! For types with conditional block participation, switch to independent data mode.
     ! This allows only some blocks/processors to write without deadlocking collective I/O.
@@ -216,6 +257,7 @@ contains
       write(*, *) "ModOutputNetCDF: nfmpi_close failed: ", nfmpi_strerror(ierr)
     ncid = -1
     if (allocated(varids)) deallocate(varids)
+    if (allocated(coordids)) deallocate(coordids)
 #endif
   end subroutine netcdf_close_file
 
@@ -237,15 +279,19 @@ contains
   !   2D: no ghost cells (nZ=1); write to (lon×lat) var
   ! ==================================================================
   subroutine netcdf_write_block(iUnit, buffer, nV, nX, nY, nZ, iBLK)
+    use ModGITM, only: Longitude, Latitude, Altitude_GB
+    use ModConst, only: cRadToDeg
     integer, intent(in) :: iUnit, nV, nX, nY, nZ, iBLK
     real, intent(in) :: buffer(nV, nX, nY, nZ)
 
 #ifdef HavePNetCDF
     integer :: iBlockLon_0, iBlockLat_0
     integer(kind=MPI_OFFSET_KIND) :: start(3), cnt(3)
+    integer(kind=MPI_OFFSET_KIND) :: cstart(1), ccnt(1)
     integer :: nLi, nLai, nAi, lo_x, hi_x, lo_y, hi_y, lo_z, hi_z
     integer :: ndimid, iV, ierr
     real, allocatable :: slice(:, :, :)
+    real(kind=8), allocatable :: coord1d(:)
 
     if (ncid == -1) return
 
@@ -277,10 +323,9 @@ contains
 
     allocate(slice(nLi, nLai, nAi))
 
-    ! For magnetic grid types, use non-collective I/O to avoid MPI deadlock
-    ! (only block 1 has data to write, but all processes must reach close_file() together).
     if (nc_needsIndepWrite) then
-      ! Non-collective put for magnetic grid types
+      ! For magnetic grid types, use non-collective I/O to avoid MPI deadlock
+      ! (only block 1 has data to write, but all processes must reach close_file() together).
       do iV = 1, nV
         slice = buffer(iV, lo_x:hi_x, lo_y:hi_y, lo_z:hi_z)
         ierr = nfmpi_put_vara_double(ncid, varids(iV), &
@@ -299,6 +344,33 @@ contains
           write(*, *) "netcdf_write_block: put_vara_double_all failed for var ", iV, &
           ": ", nfmpi_strerror(ierr)
       enddo
+      ! --- 1-D coordinate variables --- (Not present in magnetic/needsIndepWrite outputs)
+      ! lon: each block writes its own interior lon range (degrees east)
+      cstart(1) = start(1)
+      ccnt(1) = cnt(1)
+      allocate(coord1d(nLi))
+      coord1d = real(Longitude(1:nLi, 1), kind=8) * cRadToDeg
+      ierr = nfmpi_put_vara_double_all(ncid, coordids(1), cstart, ccnt, coord1d)
+      deallocate(coord1d)
+
+      ! lat: each block writes its own interior lat range (degrees north)
+      cstart(1) = start(2)
+      ccnt(1) = cnt(2)
+      allocate(coord1d(nLai))
+      coord1d = real(Latitude(1:nLai, 1), kind=8) * cRadToDeg
+      ierr = nfmpi_put_vara_double_all(ncid, coordids(2), cstart, ccnt, coord1d)
+      deallocate(coord1d)
+
+      ! z (3D only): all blocks cover the same altitude levels (km)
+      if (nc_nDims == 3) then
+        cstart(1) = 1_MPI_OFFSET_KIND
+        ccnt(1) = cnt(3)
+        allocate(coord1d(nAi))
+        coord1d = real(Altitude_GB(1, 1, 1:nAi, 1), kind=8) / 1000.0d0
+        ierr = nfmpi_put_vara_double_all(ncid, coordids(3), cstart, ccnt, coord1d)
+        deallocate(coord1d)
+      endif
+
     endif
 
     deallocate(slice)
