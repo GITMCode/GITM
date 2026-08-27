e3fc2ccf2d4fe4312f47467ca1863ac89bf8582e
Aaron Bukowski <abukowski21@gmail.com>
2026-08-27 13:51:56 -0400

    (DOC) Formatting fixes in run_information.txt

--- src/logfile.f90
+++ src/logfile.f90
@@ -503,6 +503,7 @@ subroutine write_code_information(dir)
 
     write(iCodeInfoFileUnit_, *) "#DON4SHACK"
     write(iCodeInfoFileUnit_, *) DoN4SHack
+    write(iCodeInfoFileUnit_, *) ""
 
     write(iCodeInfoFileUnit_, *) '#MSIS21'
     write(iCodeInfoFileUnit_, *) UseMsis21
@@ -613,8 +614,10 @@ subroutine write_code_information(dir)
     write(iCodeInfoFileUnit_, *) "IncludeEclipse", IncludeEclipse
     write(iCodeInfoFileUnit_, *) ""
     write(iCodeInfoFileUnit_, *) "#ECLIPSE"
-    write(iCodeInfoFileUnit_, *) EclipseStartTime
-    write(iCodeInfoFileUnit_, *) EclipseEndTime
+    call time_real_to_int(EclipseStartTime, iTime)
+    write(iCodeInfoFileUnit_, *) iTime
+    call time_real_to_int(EclipseEndTime, iTime)
+    write(iCodeInfoFileUnit_, *) iTime
     write(iCodeInfoFileUnit_, *) EclipseStartY
     write(iCodeInfoFileUnit_, *) EclipseStartZ
     write(iCodeInfoFileUnit_, *) EclipseEndY
